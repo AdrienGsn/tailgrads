3c53486b875f776d6003af70f4efeb6d8d656556
AdrienGsn <06.volts-papier@icloud.com>
2024-04-29 02:54:24 +0200

    Added ads

--- src/app/[locale]/_components/ads.tsx
+++ src/app/[locale]/_components/ads.tsx
@@ -1,23 +1,25 @@
 "use client";
 
+import { Adsense } from "@ctrl/react-adsense";
+
 import { Layout, LayoutContent } from "@/components/page/layout";
+import ENV from "@/lib/env";
 
 export const Ads = () => {
     return (
         <Layout>
-            <LayoutContent className="my-4 bg-red-400">
-                ADS
-                {/* {process.env.NODE_ENV === "production" && (
-                        <div>
-                            <Adsense
-                                client={ENV.ADSENSE_CLIENT_ID}
-                                slot="7259870550"
-                                style={{ display: "block" }}
-                                layout="in-article"
-                                format="fluid"
-                            />
-                        </div>
-                    )} */}
+            <LayoutContent className="my-4">
+                {process.env.NODE_ENV === "production" && (
+                    <div>
+                        <Adsense
+                            client={ENV.ADSENSE_CLIENT_ID}
+                            slot="7259870550"
+                            style={{ display: "block" }}
+                            layout="in-article"
+                            format="fluid"
+                        />
+                    </div>
+                )}
             </LayoutContent>
         </Layout>
     );
